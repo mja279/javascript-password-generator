b8bc25706a9521724c750eb1120e00c59df9d339
Kira <kira@dogmail.com>
2020-06-28 17:26:10 -0400

    update.v6

--- script.js
+++ script.js
@@ -27,3 +27,31 @@ console.log(useUppercase)
 //     prompt("Oops! Prompt Console is cAsE sEnSiTiVe... feel free to enter any characters to continue or Press 'Ok'.");
 // }
 
+function generate(){
+    let complexity = document.getElementById("slider").value;
+
+    // possible pw values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    let password = "";
+
+    // create for loop to choose pw characters
+    for(var i=0; i<= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+
+    //add pw to textbox/display area
+    document.getElementById("display").value = password;
+
+    //add pw to prev generated pw section
+    document.getElementById("lastNums").innerHTML += password + "<br />";
+}
+
+    //copy to clipboard bootstrap button
+    function copyPassword(){
+        document.getElementById("display").select();
+
+        document.execCommand("Copy");
+
+        alert("Password copied to clipboard!");
+    }
